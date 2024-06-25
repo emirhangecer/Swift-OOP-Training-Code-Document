08b0cd55826b92e7680b468a487f4a5631118c91
emirhangecer <emirhan19072121@gmail.com>
2024-06-25 21:53:53 +0300

    new code

--- Contents.swift
+++ Contents.swift
@@ -101,3 +101,95 @@ print("Gelen Toplam: \(gelenToplam)")
 beyza.carpma(sayi1: 20, sayi2: 10)
 
 beyza.carpma(sayi1: 10, sayi2: 10, isim: "beyza")
+
+//Static Değişkenler ve metodlar
+
+class Asinifi {
+    var x = 10
+    
+    func metod() {
+        print("metod çalıştı")
+    }
+}
+
+let a = Asinifi()
+
+print(a.x)
+
+a.metod()
+
+
+//Enumeration
+
+enum KonserveBoyut {
+    case kucuk
+    case orta
+    case buyuk
+}
+
+func ucretiHesapla(boyut: KonserveBoyut, adet: Int){
+    switch boyut {
+    case .kucuk: print("Fiyat \(adet * 13)₺")
+    case .orta: print("Fiyat \(adet * 24)₺")
+    case .buyuk: print("Fiyat \(adet * 45)₺")
+    }
+}
+
+ucretiHesapla(boyut: .orta, adet: 100)
+
+//Composition
+class Kategoriler {
+    
+    var kategori_id: Int?
+    var kategori_ad: String?
+    
+    init(kategori_id: Int, kategori_ad: String) {
+        self.kategori_id = kategori_id
+        self.kategori_ad = kategori_ad
+    }
+}
+
+class Yonetmenler {
+    var yonetmen_id: Int?
+    var yönetmen_ad: String?
+    
+    init(yonetmen_id: Int, yönetmen_ad: String) {
+        self.yonetmen_id = yonetmen_id
+        self.yönetmen_ad = yönetmen_ad
+    }
+}
+
+class Filmler{
+    
+    var film_id:Int?
+    var film_ad:String?
+    var film_yil:Int?
+    var kategori:Kategoriler?
+    var yonetmen:Yonetmenler?
+    
+    init(film_id: Int, film_ad: String, film_yil: Int, kategori: Kategoriler, yonetmen: Yonetmenler) {
+        self.film_id = film_id
+        self.film_ad = film_ad
+        self.film_yil = film_yil
+        self.kategori = kategori
+        self.yonetmen = yonetmen
+    }
+}
+
+let k1 = Kategoriler(kategori_id: 1, kategori_ad: "Dram")
+let k2 = Kategoriler(kategori_id: 2, kategori_ad: "Komedi")
+let k3 = Kategoriler(kategori_id: 3, kategori_ad: "Bilim Kurgu")
+
+let y1 = Yonetmenler(yonetmen_id: 1, yönetmen_ad: "Nuri Bilge Ceylan")
+let y2 = Yonetmenler(yonetmen_id: 2, yönetmen_ad: "Quantin Trantino")
+let y3 = Yonetmenler(yonetmen_id: 3, yönetmen_ad: "Christoper Nolan")
+
+
+let f1 = Filmler(film_id: 1, film_ad: "Django", film_yil: 2006, kategori: k1, yonetmen: y2)
+
+print("Film id: \(f1.film_id!)")
+print("Film ad: \(f1.film_ad!)")
+print("Film yıl: \(f1.film_yil!)")
+print("Film kategori: \(f1.kategori!.kategori_ad!)")
+print("Film yönetmeni: \(f1.yonetmen!.yönetmen_ad!)")
+
